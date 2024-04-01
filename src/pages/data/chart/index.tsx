a89682853cd34297b858ece9988f6c8c38c4feac
Yichend <957137116@qq.com>
2024-04-01 17:29:11 +0800

    add some Form

--- src/pages/data/chart/index.tsx
+++ src/pages/data/chart/index.tsx
@@ -1,24 +1,31 @@
-import { Button, Card, Form, Input, Modal, Select, Space } from 'antd';
-import { useState } from 'react';
 import { Line } from '@antv/g2plot';
-import { useEffect, useRef } from 'react';
+import { Breadcrumb, Button, Card, Form, Input, message, Modal, Select, Space } from 'antd';
+import { useEffect, useRef, useState } from 'react';
 
 const { Option } = Select;
 
-const ChartForm = ({ visible, onCreate, onCancel }) => {
+const ChartForm = ({ visible, onCreate, onCancel, closeModal }) => {
   const [form] = Form.useForm();
   const [xAxis, setXAxis] = useState(null);
   const [yAxis, setYAxis] = useState(null);
+  const [dataSource, setDataSource] = useState(null);
+  const [step, setStep] = useState(1);
+  const [modalTitle, setModalTitle] = useState('请选择数据源');
+  const [formData, setFormData] = useState({});
+  const [breadCrumbItems, setBreadCrumbItems] = useState({});
 
-  const handleCreate = () => {
-    form
-      .validateFields()
-      .then((values) => {
-        form.resetFields();
-        console.log('data: ', { ...values, xAxis, yAxis });
-        onCreate({ ...values, xAxis, yAxis });
-      })
-      .catch((err) => console.error('Validation failed:', err));
+  // const handleCreate = () => {
+  //   form
+  //     .validateFields()
+  //     .then((values) => {
+  //       form.resetFields();
+  //       console.log('data: ', { ...values, xAxis, yAxis });
+  //       onCreate({ ...values, xAxis, yAxis });
+  //     })
+  //     .catch((err) => console.error('Validation failed:', err));
+  // };
+  const handleDataSourceChange = (value) => {
+    setDataSource(value);
   };
 
   const handleXAxisChange = (value) => {
@@ -29,32 +36,145 @@ const ChartForm = ({ visible, onCreate, onCancel }) => {
     setYAxis(value);
   };
 
+  const handleNext = () => {
+    if (step === 1) {
+      // 此处可以添加表单验证逻辑
+      form
+        .validateFields()
+        .then((values) => {
+          console.log('vvvvvalues: ', values)
+          let breadCrumb = '';
+          switch (values.dataSource) {
+            case 'visited_behavior':
+              breadCrumb = '浏览行为表';
+              break;
+            case 'purchase_behavior':
+              breadCrumb = '购买行为表';
+              break;
+            case 'search_behavior':
+              breadCrumb = '搜索行为表';
+              break;
+            case 'comment_behavior':
+              breadCrumb = '评论行为表';
+              break;
+            case 'interact_behavior':
+              breadCrumb = '互动行为表';
+              break;
+          }
+          setFormData({ ...formData, ...values });
+          setBreadCrumbItems({ title: breadCrumb });
+          setStep(2);
+          setModalTitle('请完善以下内容');
+        })
+        .catch(() => {
+          message.error('请填写表单内容');
+        });
+    } else {
+      form
+        .validateFields()
+        .then((values) => {
+          form.resetFields();
+          console.log('data: ', { ...values, xAxis, yAxis });
+          onCreate({ ...values, xAxis, yAxis });
+          message.success('表单提交成功！');
+          setStep(1);
+          closeModal();
+        })
+        .catch((err) => console.error('Validation failed:', err));
+      form.resetFields();
+    }
+  };
+
+  const CancelButtonClick = () => {
+    if (step === 1) {
+      closeModal();
+      setBreadCrumbItems({});
+      form.resetFields();
+    } else {
+      setBreadCrumbItems({});
+      setStep(1);
+      setModalTitle('请选择数据源');
+    }
+  };
+
+  const breadCrumbInStep2 = () => {
+    let breadCrumb = '';
+    switch (formData) {
+      case 'visited_behavior':
+        breadCrumb = '浏览行为表';
+        break;
+      case 'purchase_behavior':
+        breadCrumb = '购买行为表';
+        break;
+      case 'search_behavior':
+        breadCrumb = '搜索行为表';
+        break;
+      case 'comment_behavior':
+        breadCrumb = '评论行为表';
+        break;
+      case 'interact_behavior':
+        breadCrumb = '互动行为表';
+        break;
+    }
+    return breadCrumb;
+  };
+
   return (
     <Modal
       open={visible}
-      title="Create Chart"
+      title={modalTitle}
       okText="Create"
       onCancel={onCancel}
-      onOk={handleCreate}
+      // onOk={handleCreate}
+      footer={[
+        <Button key="back" onClick={CancelButtonClick}>
+          {step === 1 ? '取消' : '上一步'}
+        </Button>,
+        <Button key="next" type="primary" onClick={handleNext}>
+          {step === 1 ? '下一步' : '完成'}
+        </Button>,
+      ]}
     >
-      <Form form={form} layout="vertical" name="form_in_modal">
-        <Form.Item name="chartName" label="Chart Name" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name="xAxis" label="X Axis" rules={[{ required: true }]}>
-          <Select onChange={handleXAxisChange}>
-            <Option value="visited_time">Visited Time</Option>
-            <Option value="visited_target">Visited Target</Option>
-            {/* Add more options as needed */}
-          </Select>
-        </Form.Item>
-        <Form.Item name="yAxis" label="Y Axis" rules={[{ required: true }]}>
-          <Select onChange={handleYAxisChange}>
-            <Option value="duration_minutes">Duration (minutes)</Option>
-            {/* Add more options as needed */}
-          </Select>
-        </Form.Item>
-      </Form>
+      {step === 1 ? (
+        <Form form={form} layout="vertical" name="form_selectDataSource">
+          <Form.Item name="dataSource" label="数据源" rules={[{ required: true }]}>
+            <Select placeholder="请选择数据源" onChange={handleDataSourceChange}>
+              <Option value="visited_behavior">浏览行为表</Option>
+              <Option value="purchase_behavior">购买行为表</Option>
+              <Option value="search_behavior">搜索行为表</Option>
+              <Option value="comment_behavior">评论行为表</Option>
+              <Option value="interact_behavior">互动行为表</Option>
+            </Select>
+          </Form.Item>
+        </Form>
+      ) : (
+        <>
+          <Breadcrumb style={{margin: '5px'}} separator=">" items={[breadCrumbItems, {title: ''}]} />
+          <Form form={form} layout="vertical" name="form_in_modal">
+            <Form.Item name="chartName" label="图表名称" rules={[{ required: true }]}>
+              <Input placeholder="请填写图表名称" />
+            </Form.Item>
+            <Form.Item name="chartType" label="图表类型" rules={[{ required: true }]}>
+              <Select placeholder="请选择图表类型" onChange={handleXAxisChange}>
+                <Option value="visited_time">折线图</Option>
+                <Option value="visited_target">柱状图</Option>
+                <Option value="visited_target">饼图</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item name="xAxis" label="X轴数据" rules={[{ required: true }]}>
+              <Select placeholder="请选择x轴数据" onChange={handleXAxisChange}>
+                <Option value="visited_time">Visited Time</Option>
+                <Option value="visited_target">Visited Target</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item name="yAxis" label="Y轴数据" rules={[{ required: true }]}>
+              <Select placeholder="请选择y轴数据" onChange={handleYAxisChange}>
+                <Option value="duration_minutes">Duration (minutes)</Option>
+              </Select>
+            </Form.Item>
+          </Form>
+        </>
+      )}
     </Modal>
   );
 };
@@ -78,6 +198,7 @@ const ChartComponent = ({ chartData }) => {
   //   smooth: true,
   // };
   useEffect(() => {
+    // 渲染图表
     if (chartContainer.current) {
       const data = [
         { year: '1991', value: 3 },
@@ -90,18 +211,16 @@ const ChartComponent = ({ chartData }) => {
         { year: '1998', value: 9 },
         { year: '1999', value: 13 },
       ];
-      
+
       const linePlot = new Line(chartContainer.current, {
         data,
         xField: 'year',
         yField: 'value',
       });
-      
+
       linePlot.render();
     }
   }, []);
-
-
 
   console.log('data in ChartComponent chartData: ', chartData);
 
@@ -120,6 +239,7 @@ const ChartPage = () => {
   const [visible, setVisible] = useState(false);
   const [chartData, setChartData] = useState(null);
 
+  // 填完表单后发送请求给后端拿数据
   const handleCreate = (values) => {
     console.log('valuse in handleCreate: ', values);
     const dummyData = {
@@ -138,6 +258,10 @@ const ChartPage = () => {
     setVisible(true);
   };
 
+  const closeModal = () => {
+    setVisible(false);
+  };
+
   const handleCancel = () => {
     setVisible(false);
   };
@@ -147,7 +271,12 @@ const ChartPage = () => {
       <Button type="primary" onClick={showModal}>
         New Chart
       </Button>
-      <ChartForm visible={visible} onCreate={handleCreate} onCancel={handleCancel} />
+      <ChartForm
+        visible={visible}
+        onCreate={handleCreate}
+        onCancel={handleCancel}
+        closeModal={closeModal}
+      />
       {chartData && <ChartComponent chartData={chartData} />}
     </div>
   );
